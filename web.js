2edd05ad4db6725932763ad725aef7a4f4528ec0
Ben Couste <ben@team.shoeboxed.com>
2014-04-07 15:13:11 -0400

    adds simple example for routing regexes

--- web.js
+++ web.js
@@ -6,6 +6,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get(/^\/love\/(\w+)$/, function(request, response) {
+	response.send('I love ' + request.params[0])
+});
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
